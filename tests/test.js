204eca1c73932029909f4cdbfb28c3d12f6ffb6c
Cesar Carruitero <cesar@mozilla.pe>
2013-02-23 16:52:13 -0500

    tests descriptions

---
+++ tests/test.js
@@ -0,0 +1,55 @@
+var server = require('../server/server'),
+    game = require('../client/js/game');
+
+describe('Game', function(){
+  describe('singlePlayer', function(){
+    /* TODO:
+    it('should select a car', function(){
+    });
+    it('should select level(race track)', function(){
+    });
+    it('should customise car', function(){
+    });
+    it('should have assignments', function(){
+    });
+    it('assigments change depends level', function(){
+    });
+    it('can win passengers to another competitor', function(){
+    });
+    it('should check before Timer ends', function(){
+    });
+    it('should at least have 1% of live', function(){
+    });
+    it('velocimeter show current velocity', function(){
+    });
+    it('state show current live state', function(){
+    });
+    it('ranking shows who win more money', function(){
+    });
+    it('should lost money when crash to anothers competitor', function(){
+    });
+    it('can insert new username', function(){
+    });
+    */
+    it('initGame', function(){
+    });
+    it('newGame', function(){
+    });
+    it('new username', function(){
+    });
+    it('endGame', function(){
+    });
+  });
+});
+
+describe('Timer', function(){
+
+});
+
+describe('player', function(){
+
+});
+
+describe('velocimeter', function(){
+
+});
